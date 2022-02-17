dd9bb07b8e97cafa5a4e9d589ea92e4c3a3b7ebc
saif siddiqui <saifsms91@rediffmail.com>
2022-02-17 23:23:27 +0800

    strategy code

--- src/main/java/com/practice/seleniumdesign/factory/GoogleEnglish.java
+++ src/main/java/com/practice/seleniumdesign/factory/GoogleEnglish.java
@@ -1,5 +1,6 @@
 package com.practice.seleniumdesign.factory;
 
+import com.google.common.util.concurrent.Uninterruptibles;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -7,42 +8,47 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
-public class GoogleEnglish extends GooglePage{
+class GoogleEnglish extends GooglePage {
 
-    private WebDriver driver;
-    private WebDriverWait wait;
+    protected WebDriver driver;
+    protected WebDriverWait wait;
 
     @FindBy(name = "q")
     private WebElement searchBox;
 
-    @FindBy(name = "btnk")
+    @FindBy(name = "btnK")
     private WebElement searchBtn;
 
 
-    @FindBy(css = "div.rc")
-    private List<WebElement> result;
+    @FindBy(css = "div.ULSxyf")
+    private List<WebElement> results;
 
-    public GoogleEnglish(final WebDriver driver){
-        this.driver= driver;
-        this.wait = new WebDriverWait(driver,30);
-        PageFactory.initElements(driver,this);
+    public GoogleEnglish(final WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, 30);
+        PageFactory.initElements(driver, this);
     }
+
     @Override
     public void launchSite() {
-this.driver.get("https://www.google.com");
+        this.driver.get("https://www.google.com");
     }
 
     @Override
     public void search(String keyword) {
-this.searchBox.sendKeys(keyword);
-this.wait.until((d)->searchBtn.isDisplayed());
-this.searchBtn.click();
+        for (char ch : keyword.toCharArray()) {
+            Uninterruptibles.sleepUninterruptibly(20, TimeUnit.MILLISECONDS);
+            this.searchBox.sendKeys(ch + "");
+        }
+        this.wait.until((d) -> searchBtn.isDisplayed());
+        this.searchBtn.click();
     }
 
     @Override
     public int getResultCount() {
-        this.wait.until((d) -> this.result.size() >1);
-        return this.result.size();
+        this.wait.until((d) -> this.results.size() > 1);
+        return this.results.size();
     }
 }
